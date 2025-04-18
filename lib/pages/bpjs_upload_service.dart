aac608a0dff5e6e12f327dd5c461068b938595fa
Jannz <fauzanyudistira29@gmail.com>
2025-04-18 10:12:34 +0700

    Add files via upload

---
+++ lib/pages/bpjs_upload_service.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+Future<void> uploadBpjsDocument({
+  required int idEmployee,
+  required String anggotaBpjs,
+  required String fieldName, // contoh: urlKk atau urlSuratNikah
+  required File file,
+}) async {
+  final uri = Uri.parse('http://213.35.123.110:5555/api/Bpjs');
+
+  var request = http.MultipartRequest('POST', uri);
+  request.fields['idEmployee'] = idEmployee.toString();
+  request.fields['anggotaBpjs'] = anggotaBpjs;
+  request.fields['tglPengajuan'] = DateTime.now().toIso8601String();
+  request.fields[fieldName] = file.path;
+
+  request.files.add(await http.MultipartFile.fromPath(fieldName, file.path));
+
+  var response = await request.send();
+
+  if (response.statusCode == 200) {
+    print("Upload berhasil!");
+  } else {
+    print("Gagal upload: ${response.statusCode}");
+  }
+}
